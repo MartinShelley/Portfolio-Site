8ce77da2ee4ea623a42ff8a1acacff59efb49adf
MartinShelley <martin.shelley91@gmail.com>
2024-03-19 09:46:01 +0000

    Updating copy on My Experience section

--- src/components/Experience/MyTimeline.vue
+++ src/components/Experience/MyTimeline.vue
@@ -2,13 +2,11 @@
   <section id="my-career-timeline">
     <h2>My Timeline</h2>
     <div class="jobs">
-      <div class="job-card" v-for="(value, key) in roles" :key="key" :style="{ 'top': `calc(${key * -5}px)` }">
+      <div class="job-card" v-for="(value, key) in roles.slice().reverse()" :key="key"
+        :style="{ 'top': `calc(${key * -5}px)` }">
         <h4 class="job-title">{{ value.jobTitle }} @ {{ value.company }}</h4>
         <p class="job-duration">{{ value.duration }}</p>
         <p class="job-description" v-if="value.description">{{ value.description }}</p>
-        <ul>
-          <li v-for="(duty, index) in value.duties" :key="index">{{ duty }}</li>
-        </ul>
         <div class="skills">
           <li v-for="( skill, index ) in value.skills" :key="index">{{ skill }}</li>
         </div>
@@ -27,42 +25,37 @@ export default {
           jobTitle: 'Sales and Application Specialist',
           duration: 'Aug 2014 - May 2016',
           skills: ['HTML', 'CSS', 'Wordpress'],
-          description: 'Fresh out of university, I landed my first role in technical customer support at Realtime DC where some of my duties included:',
-          duties: ['Using WordPress/CPanel to maintain UK website and to develop the USA website', 'Configure product setup & 1st/2nd line support for customers using Real-Link system'],
+          description: 'Using WordPress/CPanel to maintain UK website and to develop the USA website. Configure product setup & provide 1st/2nd line support for customers using Real-Link system.',
         },
         {
           company: 'Lynchpin Analytics',
           jobTitle: 'Data Engineer',
           duration: 'May 2016 - Oct 2018',
-          skills: ['HTML', 'CSS', 'Javascript'],
-          // duties: ['Leading on the implementation of A/B tests for international clients', 'Led on other Analytics projects for big clients such as Ticketmaster & Hearst Magazines'],
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          skills: ['HTML', 'CSS', 'Javascript', 'jQuery'],
+          description: 'Leading on the implementation of A/B tests for international clients. Led on other Analytics projects for big clients such as Ticketmaster & Hearst Magazines. Using Javascript to help implement and fix Analytics features on clients websites.'
         },
         {
           company: 'Racing Post',
           jobTitle: 'Implementation Specialist',
           duration: 'Dec 2018 - Oct 2021',
-          skills: ['HTML', 'CSS', 'Javascript'],
-          // duties: ['Using Vanilla JS to add analytics tracking and embed 3rd party applications to Racing Post site', 'Spearheaded the integration of various third-party tools', 'Diagnosed issues and developed technical solutions (using Vanilla Javascript) for third-party tool integrations'],
-          // tools: ['HTML', 'CSS', 'Javascript', 'Google Analytics', 'Google Tag Manager', 'Segment']
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          skills: ['HTML', 'CSS', 'Javascript', 'jQuery', 'Google Analytics', 'Google Tag Manager', 'Segment'],
+          description: 'Using Vanilla Javascript to add analytics tracking and embed 3rd party applications to Racing Post site. Spearheaded the integration of various third-party tools through Racing Posts Customer Data Platform. Diagnosed issues and developed technical solutions (using Vanilla Javascript) for third-party tool integrations.'
         },
         {
           company: 'The White Company',
           jobTitle: 'Front End Developer',
           duration: 'Nov 2021 - Aug 2023',
-          skills: ['HTML', 'CSS', 'SASS', 'Javascript'],
-          description: 'During COVID, I decided I wanted to get into Web Development & I felt the best route for me to get into Web Development was to lean on my A/B testing/CRO experience. At The White Company, I was the lead CRO developer leading on a comprehensive testing & personalisation roadmap. I also:'
+          skills: ['HTML', 'CSS', 'SASS', 'Javascript', 'jQuery', 'Adobe Target', 'Adobe Launch'],
+          description: 'Lead CRO developer at The White Company. Led on a comprehensive testing & personalisation roadmap where one test equated to an £1.8m annual incremental revenue benefit. Acted as main point of contact for technical & non-technical senior stakeholders. Worked closely with Product & Designers to implement their ideas onto the site & also ensured that crucial web features were rolled out onto the site gradually to allow for testing.'
         },
         {
           company: 'I-Neda',
           jobTitle: 'Front End Developer',
           duration: 'Sep 2023 - Present',
           skills: ['HTML', 'CSS', 'SASS', 'Javascript', 'Angular', 'Typescript', 'Nativescript'],
-          description: 'Continuing my Web Development journey, I wanted to work more with Front End frameworks and continue to develop as a Front End Developer and learn even more skills. At I-Neda I have:'
+          description: 'Build and maintain Angular components across different client frontends. Led on a client project which introduced QR code scanning for a clients customers to enable them to scan their race tickets. Proactively addressed and resolved bugs, contributing to the overall stability and functionality of multiple client applications.'
         }
       ],
-      // years: ['2014', '2016', '2018', '2021', '2023']
     }
   },
   computed: {
